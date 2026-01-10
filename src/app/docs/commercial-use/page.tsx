b3fdf6352f64ec109bae98abbdc5d289c59bbbe4
Aiken Tine Ahac <ahac.aiken@gmail.com>
2026-01-10 15:35:16 +0100

    Remove unused code

--- src/app/docs/commercial-use/page.tsx
+++ src/app/docs/commercial-use/page.tsx
@@ -2,72 +2,16 @@ import {
   DocsCode,
   DocsLayout,
   DocsLink,
-  DocsNote,
   DocsSection,
 } from "../_components/docs";
 import { CodeBlock } from "../_components/code-block";
 import { Metadata } from "next";
 
-const installMapCode = `npx @react-native-reusables/cli@latest add https://mapcn-rn.aiken.si/maps/map.json`;
 const installMaptilerMapCode = `npx @react-native-reusables/cli@latest add https://mapcn-rn.aiken.si/maps/map-maptiler.json`;
 
 const maptilerApiKeyCode = `# .env
 EXPO_PUBLIC_MAPTILER_API_KEY=your_maptiler_api_key_here
 `;
-
-const installDepsCode = `npx expo install @maplibre/maplibre-react-native@11.0.0-alpha.28 expo-location`;
-
-const setupPermissionsExpo = `<!-- app.json -->
-{
-  "expo": {
-    ...
-    "newArchEnabled": true,
-    "ios": {
-      ...
-      "infoPlist": {
-        "ITSAppUsesNonExemptEncryption": false,
-        "NSAppTransportSecurity": {
-          "NSAllowsArbitraryLoads": true
-        },
-        "NSLocationWhenInUseUsageDescription": "This app needs access to your location to show you on the map.",
-        "NSLocationAlwaysAndWhenInUseUsageDescription": "This app needs access to your location to show you on the map."
-      }
-    },
-    "android": {
-      ...
-      "permissions": [
-        "ACCESS_FINE_LOCATION",
-        "ACCESS_COARSE_LOCATION"
-      ]
-    },
-    "plugins": [
-      ...
-      // this will show a warning for no valid plugin, but it is required
-      "@maplibre/maplibre-react-native"
-    ],
-    ...
-  }
-}
-`;
-
-const setupPermissionsIOS = `<!-- ios/YourApp/Info.plist -->
-<key>NSLocationWhenInUseUsageDescription</key>
-<string>We need your location to show it on the map</string>`;
-
-const setupPermissionsAndroid = `<!-- android/app/src/main/AndroidManifest.xml -->
-<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
-<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />`;
-
-const usageCode = `import { Map } from "@/components/ui/map";
-import { View } from "react-native";
-
-export default function BasicMapExample() {
-  return (
-    <View className="h-[500px] rounded-xl overflow-hidden border border-border">
-      <Map zoom={12} center={[-122.4194, 37.7749]} />
-    </View>
-  );
-}`;
 
 export const metadata: Metadata = {
   title: "Installation",
